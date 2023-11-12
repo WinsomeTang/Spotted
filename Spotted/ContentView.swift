32e6cd021494dcd650747461d25a230eee8dbd35
Winsome Tang <tangwins@sheridancollege.ca>
2023-11-11 22:24:00 -0500

    Initial Commit

---
+++ Spotted/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Spotted
+//
+//  Created by Winsome Tang on 2023-11-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Spotted/SpottedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpottedApp.swift
+//  Spotted
+//
+//  Created by Winsome Tang on 2023-11-11.
+//
+
+import SwiftUI
+
+@main
+struct SpottedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
