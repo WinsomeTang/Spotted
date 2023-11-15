c92d996432ed3c8d9cf1de859c60ffeeb9f50518
Winsome Tang <tangwins@sheridancollege.ca>
2023-11-15 12:26:15 -0500

    Retry configuring firebase in project

--- Spotted/SpottedApp.swift
+++ Spotted/SpottedApp.swift
@@ -7,27 +7,16 @@
 import SwiftUI
 import FirebaseCore
 
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-      print("Configured Firebase!")
-    return true
-  }
-}
-
 @main
 struct SpottedApp: App {
-  // register app delegate for Firebase setup
-  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-
-
+    init(){
+        FirebaseApp.configure()
+        print("Configured Firebase! IT WORKSs")
+    }
+    
   var body: some Scene {
     WindowGroup {
-      NavigationView {
         ContentView()
-      }
     }
   }
 }
