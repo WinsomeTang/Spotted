ee2e2a78ed7e733ab13449ee4881493c7ed81e77
Winsome Tang <tangwins@sheridancollege.ca>
2023-11-15 11:14:25 -0500

    Attempt to add plist again

--- Spotted/SpottedApp.swift
+++ Spotted/SpottedApp.swift
@@ -4,22 +4,30 @@
 //
 //  Created by Winsome Tang on 2023-11-11.
 //
-
 import SwiftUI
 import FirebaseCore
 
 
-@main
-struct SpottedApp: App {
-    init(){
-        FirebaseApp.configure()
-    }
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
 }
 
+@main
+struct SpottedApp: App {
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
 
-      
+
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        ContentView()
+      }
+    }
+  }
+}
