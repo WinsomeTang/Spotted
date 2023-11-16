34600a67ea1c38e17840ecf914c55bea1d52b2a1
Michael Werbowy <werbowym@sheridancollege.ca>
2023-11-15 21:29:23 -0500

    added tab view for map, activity, account views. bug fixed mapview

--- Spotted/Views/MapView.swift
+++ Spotted/Views/MapView.swift
@@ -18,22 +18,38 @@ struct MapView: View {
     @ObservedObject var viewModel = MapViewModel()
 
     var body: some View {
-        Map(position: $viewModel.position) {
-            Marker("My Location", coordinate: locationManager.location.coordinate ?? CLLocationCoordinate2D())
-            // Add other map content here
+        TabView {
+            // Map Tab
+            Map(position: $viewModel.position) {
+                Marker("My Location", coordinate: locationManager.location.coordinate ?? CLLocationCoordinate2D())
+                // Add other map content here
+            }
+            .onAppear {
+                viewModel.updatePosition(coordinate: locationManager.location.coordinate ?? CLLocationCoordinate2D())
+            }
+            .onReceive(locationManager.$location) { _ in
+                viewModel.updatePosition(coordinate: locationManager.location.coordinate ?? CLLocationCoordinate2D())
+            }
+            .navigationBarHidden(true) // Hide the navigation bar
+            .navigationBarBackButtonHidden(true) // Hide the back button
+            .tabItem {
+                Label("Map", systemImage: "map")
+            }
+
+            // Activity Tab
+            ActivityView()
+                .tabItem {
+                    Label("Activity", systemImage: "chart.bar")
+                }
+
+            // Account Tab
+            AccountView()
+                .tabItem {
+                    Label("Account", systemImage: "person")
+                }
         }
-        .onAppear {
-            viewModel.updatePosition(coordinate: locationManager.location.coordinate ?? CLLocationCoordinate2D())
-        }
-        .onReceive(locationManager.$location) { _ in
-            viewModel.updatePosition(coordinate: locationManager.location.coordinate ?? CLLocationCoordinate2D())
-        }
-        .navigationBarTitle("Map")
-        .navigationBarHidden(true) // Hide the navigation bar
-        .navigationBarBackButtonHidden(true) // Hide the back button
     }
 }
-
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
