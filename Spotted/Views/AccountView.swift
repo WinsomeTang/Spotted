54be752f6b22ba4d620476a6fdea46e13e01154f
Michael Werbowy <werbowym@sheridancollege.ca>
2023-11-15 21:21:17 -0500

    added AccountView,ActivityView, removed navview from MapContent (no back button)

---
+++ Spotted/Views/AccountView.swift
@@ -0,0 +1,8 @@
+//
+//  AccountView.swift
+//  Spotted
+//
+//  Created by Michael Werbowy on 2023-11-15.
+//
+
+import Foundation
---
+++ Spotted/Views/ActivityView.swift
@@ -0,0 +1,8 @@
+//
+//  ActivityView.swift
+//  Spotted
+//
+//  Created by Michael Werbowy on 2023-11-15.
+//
+
+import Foundation
